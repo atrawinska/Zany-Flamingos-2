1511eda16f17996ff0884f321f6ab51db1b1d3b9
atrawinska <agnieszka.alicja.trawinska@gmail.com>
2025-03-14 20:43:44 +0100

    Added classes

--- obj/Debug/net9.0/e_learning_application.AssemblyInfo.cs
+++ obj/Debug/net9.0/e_learning_application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("e_learning_application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+930a2f7802675a641d9575740c8d29e5d694dd03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f0f910c934a1a070c5d176198ce5aebc69b17c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("e_learning_application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("e_learning_application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
