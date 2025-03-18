3d92d0a8d7c7711de581031aade20b92b774d139
mikklu04 <miki.kluba@gmail.com>
2025-03-18 16:21:37 +0100

    Updated MainWindowViewModel to use TeacherInsideView and StudentInsideView

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -7,17 +7,46 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace e_learning_application.ViewModels;
 
-public partial class MainWindowViewModel : ViewModelBase
-{
-    [ObservableProperty]
-    private UserControl currentView;
-
-    private readonly MainInsideView insideView = new MainInsideView{DataContext=new MainInsideView()};
-   
-
-       public MainWindowViewModel() 
+    public partial class MainWindowViewModel : ViewModelBase
     {
-        currentView = insideView;
-    }
+        private static MainWindowViewModel instance;
+        public static MainWindowViewModel Instance => instance ??= new MainWindowViewModel();
 
-}
+        [ObservableProperty]
+        private UserControl currentView;
+
+        private readonly RoleSelectionView roleSelectionView = new RoleSelectionView { DataContext = new RoleSelectionViewModel() };
+        private readonly MainInsideView insideView = new MainInsideView { DataContext = new MainInsideView() };
+        private readonly StudentInsideView studentInsideView = new StudentInsideView { DataContext = new StudentInsideViewModel() };
+        private readonly TeacherInsideView teacherInsideView = new TeacherInsideView { DataContext = new TeacherInsideViewModel() };
+
+        public MainWindowViewModel()
+        {
+            currentView = roleSelectionView;
+        }
+
+        public void SwitchToInsideView()
+        {
+            currentView = insideView;
+        }
+
+        public void SwitchToRoleSelectionView()
+        {
+            currentView = roleSelectionView;
+        }
+
+        public void SwitchToStudentView()
+        {
+            currentView = studentInsideView;
+        }
+
+        public void SwitchToTeacherView()
+        {
+            currentView = teacherInsideView;
+        }
+
+        public void SwitchToLoginView()
+        {
+            currentView = new LoginView { DataContext = new LoginViewModel() };
+        }
+    }
